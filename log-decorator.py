b53c392815294dbdc8187774349aebba5627505f
dartmater <rainbow@bk.ru>
2025-06-25 18:58:23 -0400

    final

--- log-decorator.py
+++ log-decorator.py
@@ -1,11 +1,11 @@
 import logging
 
-# Setup logger
+
 logger = logging.getLogger(__name__ + "_parameter_log")
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.FileHandler("./decorator.log", "a"))
 
-# Decorator function
+
 def logger_decorator(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
